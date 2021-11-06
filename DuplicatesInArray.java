18f98669e14e7bd366857a6495d01bb16777627d
M-Manikandan <93439317+M-Manikandan@users.noreply.github.com>
2021-11-06 07:47:38 +0530

    Add files via upload

---
+++ DuplicatesInArray.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class DuplicatesInArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 12, 4, 2, 8, 4, 10, 12, 2, 10 };
+
+		int count;
+		int len = arr.length;
+
+		for (int i = 0; i < len; i++) {
+			count = 0;
+
+			for (int j = i; j < len - 1; j++)
+
+				if (arr[i] == arr[j])
+					count = count + 1;
+			if (count > 1)
+				System.out.println(arr[i]);
+
+		}
+
+	}
+
+}
